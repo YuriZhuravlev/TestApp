c3ea483de3da8b395fe42f8928dbd3ce9b255923
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-04-17 19:04:56 +0300

    Перенесен контейнер для фрагмента

--- Leroy/app/src/main/java/com/zhuravlev/leroy/MainActivity.kt
+++ Leroy/app/src/main/java/com/zhuravlev/leroy/MainActivity.kt
@@ -10,27 +10,22 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
     lateinit var appBarConfiguration: AppBarConfiguration
-
-    lateinit var collapsingToolbarLayout: CollapsingToolbarLayout
-    lateinit var searchToolbar: Toolbar
     lateinit var simpleToolbar: Toolbar
+
     var activeSearchToolbar: Boolean = true
 
     fun switchToolbar(searchToolbar: Boolean = false) {
         if (activeSearchToolbar != searchToolbar) {
             if (activeSearchToolbar) {
-                collapsingToolbarLayout.visibility = View.GONE
                 simpleToolbar.visibility = View.VISIBLE
                 activeSearchToolbar = false
             } else {
                 simpleToolbar.visibility = View.GONE
-                collapsingToolbarLayout.visibility = View.VISIBLE
                 activeSearchToolbar = true
             }
         }
@@ -40,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar)
         simpleToolbar = findViewById(R.id.simple_toolbar)
 
         navController = findNavController(R.id.nav_host_fragment)
