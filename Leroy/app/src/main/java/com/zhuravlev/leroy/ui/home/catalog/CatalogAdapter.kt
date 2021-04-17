424bb4346b4b6d724ef4d26924eda5eb03c4f6b9
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-04-17 20:21:27 +0300

    Доработка

--- Leroy/app/src/main/java/com/zhuravlev/leroy/ui/home/catalog/CatalogAdapter.kt
+++ Leroy/app/src/main/java/com/zhuravlev/leroy/ui/home/catalog/CatalogAdapter.kt
@@ -14,16 +14,17 @@ import com.zhuravlev.leroy.model.Catalog
 open class CatalogAdapter(val list: List<Catalog>) : Adapter<CatalogViewHolder>() {
     companion object {
         const val COMMON_TYPE = 0
-        const val SHOW_ALL_TYPE = 1
+        const val CATALOG_TYPE = 1
+        const val SHOW_ALL_TYPE = 2
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatalogViewHolder {
         return CatalogViewHolder(
             LayoutInflater.from(parent.context).inflate(
-                if (viewType == COMMON_TYPE) {
-                    R.layout.item_catalog
-                } else {
-                    R.layout.item_show_all
+                when (viewType) {
+                    CATALOG_TYPE -> R.layout.item_catalog
+                    SHOW_ALL_TYPE -> R.layout.item_show_all
+                    else -> R.layout.item_category
                 }, parent, false
             )
 
@@ -32,9 +33,8 @@ open class CatalogAdapter(val list: List<Catalog>) : Adapter<CatalogViewHolder>(
 
     override fun onBindViewHolder(holder: CatalogViewHolder, position: Int) {
         with(list[position]) {
-            val clickListener: View.OnClickListener = when (position) {
-                0 -> Navigation.createNavigateOnClickListener(R.id.action_to_tile)
-                list.lastIndex -> Navigation.createNavigateOnClickListener(R.id.action_to_tile)
+            val clickListener: View.OnClickListener = when (getItemViewType(position)) {
+                SHOW_ALL_TYPE, CATALOG_TYPE -> Navigation.createNavigateOnClickListener(R.id.action_to_tile)
                 else -> {
                     val direction = object : NavDirections {
                         override fun getActionId(): Int {
@@ -64,10 +64,10 @@ open class CatalogAdapter(val list: List<Catalog>) : Adapter<CatalogViewHolder>(
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (list.lastIndex == position) {
-            SHOW_ALL_TYPE
-        } else {
-            COMMON_TYPE
+        return when (position) {
+            0 -> CATALOG_TYPE
+            list.lastIndex -> SHOW_ALL_TYPE
+            else -> COMMON_TYPE
         }
     }
 }
