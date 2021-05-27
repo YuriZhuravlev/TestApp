b46efdd24596e1943cb2cdb9b8e4d9c923f72ae5
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-05-27 23:07:31 +0300

    Добавлен макет блюд

--- FoodViewer/app/src/main/java/com/zhuravlev/foodviewer/ui/menu/MenuFragment.kt
+++ FoodViewer/app/src/main/java/com/zhuravlev/foodviewer/ui/menu/MenuFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.zhuravlev.foodviewer.databinding.FragmentMenuBinding
 import com.zhuravlev.foodviewer.ui.common.CustomToolbarFragment
@@ -30,10 +28,10 @@ class MenuFragment : CustomToolbarFragment() {
         _binding = FragmentMenuBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textMenu
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = binding.textMenu
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 
